6cdfe767b8d39de75e499ef69f66df74d0140bad
TheRoguishFOX <mr.carte.florent@gmail.com>
2019-08-17 09:09:53 +0200

    MàJ 1.3.1
    eviter de doublé la page contact avec home

--- Controller/frontend.php
+++ Controller/frontend.php
@@ -13,8 +13,8 @@
     }
 //dans la fonction newsletter on récupère le $_POST['news_mail'] qui est dans index
 
-function newsletter($news_mail){
+    function newsletter($news_mail){
     
-    insert_newsletter($news_mail);
-    require('./view/frontend/view_contact_fr.php');
+        insert_newsletter($news_mail);
+        require('./view/frontend/view_contact_fr.php');
 }
--- index.php
+++ index.php
@@ -2,8 +2,8 @@
 
     require_once('./controller/frontend.php');
 
-
 try{
+
     if(isset($_GET['action'])){ 
         
         if($_GET['action']=='contact'){
@@ -12,17 +12,17 @@ try{
         
     }
     
-    if(isset($_GET['news'])){
+    else if(isset($_GET['news'])){
         if($_GET['news']=='inscription'){
             newsletter($_POST['news_mail']);
         }
 // dans view contact, en cliquant sur valider on récupère news_mail dont l'action est news=inscription'. cette action se transforme en fonction newsletter quon developpe dans frontend du controller   
     }
 
-    else {
-        home();
+    else home();
+    
     }
-}
-
-catch (Exception $e) {
-            echo($e->getMessage());}
+    
+    catch(Exception $e) {
+        die("Erreur :" . $e->getMessage());
+    }
