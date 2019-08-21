5c18bb950d664811e0103d717865e5db0e9e8c34
Juliuse <juliuse@hotmail.fr>
2019-08-21 16:13:50 +0200

    actus
    page actus

--- view/frontend/view_actus_fr.php
+++ view/frontend/view_actus_fr.php
@@ -7,21 +7,20 @@ $title_page = "Les actus";
 
 ob_start();
 ?>
-<section>
+<section >
     <article id="baractus">
         <div id="afterwork">
             <h2> After work &amp; Before Lunch</h2>
         </div>
 
-        <div id="texteafterwork">
+        <div class="texte">
             <p>Concerts, jeux de société...</p>
         </div>
-
     </article>
 
     <article id="degust">
 
-        <div id="textedegustation">
+        <div class="texte">
             <p>Dégustations privées, dégustations publiques</p>
         </div>
         <div id="degustation">
@@ -33,7 +32,7 @@ ob_start();
         <div id="foireauxvins">
             <h2> Foire aux vins</h2>
         </div>
-        <div id="textefoire">
+        <div class="texte">
             <p>Florent vous sélectionne une gamme de vins avec des prix défiant toute concurrence ! </p>
         </div>
     </article>
@@ -43,8 +42,8 @@ ob_start();
     <article id="fete">
         
 
-        <div id="textefete">
-            <p>Pensez à gâter vos proches avec nos coffrets et chèques cadeaux ainsi que nos offres spéciales fêtes ! <br><br> Fête des pères, Noël, anniversaires... nous serons vous conseillez pour faire de ces cadeaux des moments de fêtes inoubliables! </p>
+        <div class="texte">
+            <p>Pensez à gâter vos proches avec nos coffrets et chèques cadeaux ainsi que nos offres spéciales fêtes ! <br><br><br>Fête des pères, Noël, anniversaires... nous serons vous conseillez pour faire de ces cadeaux des moments de fêtes inoubliables! </p>
         </div>
         <div id="fetedivers">
             <h2> Spécial fêtes</h2>
@@ -55,8 +54,8 @@ ob_start();
         <div id="mariagedivers">
             <h2> Fournisseur de bonheur </h2>
         </div>
-        <div id="textemariage">
-            <p>Nous pouvons vous fournir pour tous les moments importants de votre vie ! <br> Mariages, baptêmes, anniversaire.. Pensez au meilleur, nous nous occupons du reste! Nous sélectionnerons avec vous lors d'une dégustation, toutes les boissons dont vous réviez.<br> Nous louons également des pompes à bières </p>
+        <div class="texte">
+            <p>Nous pouvons vous fournir pour tous les moments importants de votre vie ! <br><br><br> Mariages, baptêmes, anniversaire.. Pensez au meilleur, nous nous occupons du reste! Nous sélectionnerons avec vous lors d'une dégustation, toutes les boissons dont vous réviez.<br> Nous louons également des pompes à bières </p>
         </div>
 
     </article>
