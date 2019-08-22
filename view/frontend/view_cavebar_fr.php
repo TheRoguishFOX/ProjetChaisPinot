d7d293b9449ef5d5e8a0ddb705a439b1282e0b0e
TheRoguishFOX <mr.carte.florent@gmail.com>
2019-08-22 17:15:11 +0200

    Modif slide

--- view/frontend/view_cavebar_fr.php
+++ view/frontend/view_cavebar_fr.php
@@ -35,7 +35,7 @@ ob_start();
 
             /*#region responsive code begin*/
 
-            var MAX_WIDTH = 1000;
+            var MAX_WIDTH = 900;
 
             function ScaleSlider() {
                 var containerElement = jssor_1_slider.$Elmt.parentNode;
@@ -83,12 +83,12 @@ ob_start();
         </div>
     </article>
     <br>
-    <div id="jssor_1" style="position:relative;margin:0 auto;top:0px;left:0px;width:1000px;height:563px;overflow:hidden;visibility:hidden;">
+    <div id="jssor_1" style="position:relative;margin:0 auto;top:0px;left:0px;width:900px;height:450px;overflow:hidden;visibility:hidden;">
         <!-- Loading Screen -->
         <div data-u="loading" class="jssorl-009-spin" style="position:absolute;top:0px;left:0px;width:100%;height:100%;text-align:center;background-color:rgba(0,0,0,0.7);">
             <img style="margin-top:-19px;position:relative;top:50%;width:38px;height:38px;" src="img/spin.svg" />
         </div>
-        <div data-u="slides" style="cursor:default;position:relative;top:0px;left:0px;width:1000px;height:563px;overflow:hidden;">
+        <div data-u="slides" style="cursor:default;position:relative;top:0px;left:0px;width:900px;height:450px;overflow:hidden;">
             <div>
                 <img data-u="image" src="./public/images/photo/slider/20190821_150119.jpg" />
             </div>
